a31f7bab25d24ba64029341e47d7d9d6cd44d653
Melnik, Aleksandr <Aleksandr.Melnik2@T-Mobile.com>
2019-02-16 17:13:17 -0800

    add test for PropertyValueMultiSelectOptions

---
+++ opsman/product/properties/property_value_test.go
@@ -0,0 +1,76 @@
+package properties_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/pivotalservices/tile-config-generator/opsman/product/properties"
+)
+
+var _ = Describe("PropertyValueMultiSelectOptions", func() {
+	var (
+		propertyValueBytes []byte
+		propertyValue      PropertyValueMultiSelectOptions
+		err                error
+	)
+
+	BeforeEach(func() {
+		propertyValueBytes = []byte{}
+		propertyValue = PropertyValueMultiSelectOptions{}
+	})
+
+	Context("MarshalJSON", func() {
+		JustBeforeEach(func() {
+			propertyValueBytes, err = propertyValue.MarshalJSON()
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		Context("when marshaling 'non-existent-value'", func() {
+			BeforeEach(func() {
+				propertyValue.NonExistentValue = true
+			})
+
+			It("sets marshals the string 'non-existent-value'", func() {
+				Expect(propertyValueBytes).To(Equal([]byte(`"non-existent-value"`)))
+			})
+		})
+
+		Context("when marshaling some options", func() {
+			BeforeEach(func() {
+				propertyValue.NonExistentValue = false
+				propertyValue.Value = []string{"beep", "boop"}
+			})
+
+			It("marshals all the options into a list", func() {
+				Expect(propertyValueBytes).To(Equal([]byte(`["beep","boop"]`)))
+			})
+		})
+	})
+
+	Context("UnmarshalJSON", func() {
+		JustBeforeEach(func() {
+			err = propertyValue.UnmarshalJSON(propertyValueBytes)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		Context("when unmarshaling 'non-existent-value'", func() {
+			BeforeEach(func() {
+				propertyValueBytes = []byte(`"non-existent-value"`)
+			})
+
+			It("sets NonExistentValue to true", func() {
+				Expect(propertyValue.NonExistentValue).To(BeTrue())
+			})
+		})
+
+		Context("when unmarshaling some options", func() {
+			BeforeEach(func() {
+				propertyValueBytes = []byte(`["beep", "boop"]`)
+			})
+
+			It("sets NonExistentValue to true", func() {
+				Expect(propertyValue.NonExistentValue).To(BeFalse())
+				Expect(propertyValue.Value).To(ConsistOf("beep", "boop"))
+			})
+		})
+	})
+})
